79340ff0519552ee6e39831073f97f313b6cb4e9
Paulac25 <condepaula472@gmail.com>
2025-12-10 18:08:40 -0500

    add producto filtro

--- proyectoBar2/src/java/beans/ProductoBean.java
+++ proyectoBar2/src/java/beans/ProductoBean.java
@@ -1,28 +1,49 @@
 package beans;
 
 import dao.ProductoDAO;
+import java.io.Serializable;
+import java.util.ArrayList;
 import java.util.List;
-import javax.faces.bean.ManagedBean;
-import javax.faces.bean.SessionScoped;
-import javax.faces.bean.ViewScoped;
-import javax.inject.Inject;
 import javax.inject.Named;
+import javax.faces.view.ViewScoped;
 import modelo.Producto;
 
-
 @Named
-@SessionScoped
-@ManagedBean
 @ViewScoped
-public class ProductoBean {
+public class ProductoBean implements Serializable {
 
     private Producto producto = new Producto();
     private List<Producto> lista;
-      @Inject
+    private List<Producto> listaFiltrada;
+
+    private String filtroNombre = "";
+    private String filtroTipo = "";
+
     private ProductoDAO dao = new ProductoDAO();
 
     public Producto getProducto() { return producto; }
     public void setProducto(Producto producto) { this.producto = producto; }
+
+    public String getFiltroNombre() { return filtroNombre; }
+    public void setFiltroNombre(String filtroNombre) { this.filtroNombre = filtroNombre; }
+
+    public String getFiltroTipo() { return filtroTipo; }
+    public void setFiltroTipo(String filtroTipo) { this.filtroTipo = filtroTipo; }
+
+    public List<Producto> getListaFiltrada() {
+        if (listaFiltrada == null) {  
+            try {
+                listaFiltrada = dao.listar();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return listaFiltrada;
+    }
+
+    public void setListaFiltrada(List<Producto> listaFiltrada) {
+        this.listaFiltrada = listaFiltrada;
+    }
 
     public List<Producto> getLista() {
         try {
@@ -40,7 +61,10 @@ public class ProductoBean {
             } else {
                 dao.insertar(producto);
             }
-            producto = new Producto(); // limpiar
+
+            producto = new Producto(); 
+            actualizarLista();
+
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -53,6 +77,43 @@ public class ProductoBean {
     public void eliminar(int id) {
         try {
             dao.eliminar(id);
+            actualizarLista();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void filtrar() {
+        try {
+            List<Producto> todos = dao.listar();
+            listaFiltrada = new ArrayList<>();
+
+            for (Producto p : todos) {
+
+                boolean coincide = true;
+
+                if (filtroNombre != null && !filtroNombre.isEmpty()) {
+                    coincide &= p.getNombre().toLowerCase()
+                            .contains(filtroNombre.toLowerCase());
+                }
+
+                if (filtroTipo != null && !filtroTipo.isEmpty()) {
+                    coincide &= p.getTipo_producto().equalsIgnoreCase(filtroTipo);
+                }
+
+                if (coincide) {
+                    listaFiltrada.add(p);
+                }
+            }
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void actualizarLista() {
+        try {
+            listaFiltrada = dao.listar();
         } catch (Exception e) {
             e.printStackTrace();
         }
--- proyectoBar2/src/java/modelo/DetalleVenta.java
+++ proyectoBar2/src/java/modelo/DetalleVenta.java
@@ -5,50 +5,57 @@ public class DetalleVenta {
     private int id_venta;
     private int id_producto;
     private String descripcion;
-    private int cantidad_productos;
-    private double precio_unitario;
+    private Integer cantidad_productos;
+    private Double precio_unitario;
 
     public DetalleVenta() {}
 
-    public int getId_detalleV() { 
-        return id_detalleV; 
-    }
-    public void setId_detalleV(int id_detalleV) { 
-        this.id_detalleV = id_detalleV; 
+    public int getId_detalleV() {
+        return id_detalleV;
     }
 
-    public int getId_venta() { 
-        return id_venta; 
-    }
-    public void setId_venta(int id_venta) { 
-        this.id_venta = id_venta; 
+    public void setId_detalleV(int id_detalleV) {
+        this.id_detalleV = id_detalleV;
     }
 
-    public int getId_producto() { 
-        return id_producto; 
-    }
-    public void setId_producto(int id_producto) { 
-        this.id_producto = id_producto; 
+    public int getId_venta() {
+        return id_venta;
     }
 
-    public String getDescripcion() { 
-        return descripcion; 
-    }
-    public void setDescripcion(String descripcion) { 
-        this.descripcion = descripcion; 
+    public void setId_venta(int id_venta) {
+        this.id_venta = id_venta;
     }
 
-    public int getCantidad_productos() { 
-        return cantidad_productos; 
-    }
-    public void setCantidad_productos(int cantidad_productos) { 
-        this.cantidad_productos = cantidad_productos; 
+    public int getId_producto() {
+        return id_producto;
     }
 
-    public double getPrecio_unitario() { 
-        return precio_unitario; 
+    public void setId_producto(int id_producto) {
+        this.id_producto = id_producto;
     }
-    public void setPrecio_unitario(double precio_unitario) { 
-        this.precio_unitario = precio_unitario; 
+
+    public String getDescripcion() {
+        return descripcion;
     }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public Integer getCantidad_productos() {
+        return cantidad_productos;
+    }
+
+    public void setCantidad_productos(Integer cantidad_productos) {
+        this.cantidad_productos = cantidad_productos;
+    }
+
+    public Double getPrecio_unitario() {
+        return precio_unitario;
+    }
+
+    public void setPrecio_unitario(Double precio_unitario) {
+        this.precio_unitario = precio_unitario;
+    }
+    
 }
